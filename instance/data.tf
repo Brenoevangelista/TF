1ef398330d3f7e1ad08f1177862fcf00c6d378b2
Brenoevangelista <68923583+Brenoevangelista@users.noreply.github.com>
2024-06-28 09:59:29 -0300

    Update data.tf

--- instance/data.tf
+++ instance/data.tf
@@ -1,12 +1,12 @@
 data "aws_vpc" "VPC_DEVELOPMENT" {
 
-  id = "vpc-0a981cde95fdd5ebe"
+  id = "vpc"
 
 }
 
 data "aws_subnet" "DEV_SUBNET_PUBLIC_1" {
 
-  id = "subnet-028e42732b06a8901"
+  id = "subnet-"
 
 }
 
